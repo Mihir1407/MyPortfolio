dc8d9aa23cf606ddc089d7b96bf7a5c730aa8178
Mihir Chitre <chitre.m@northeastern.edu>
2024-03-05 18:18:38 -0500

    Added the 3d models for the home page

---
+++ src/models/Sky.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
+import skyScene from "../assets/3d/sky.glb";
+
+const Sky = () => {
+    const sky = useGLTF(skyScene);
+    const skyRef = useRef();
+    return (
+        <mesh ref={skyRef}>
+            <primitive object={sky.scene} />
+        </mesh>
+    )
+}
+
+export default Sky
